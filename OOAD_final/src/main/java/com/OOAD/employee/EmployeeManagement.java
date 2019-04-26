fd662fdfcc67f75fc5b5a36c509b3e428e0189b5
Sowmya Ramakrishnan <31675002+sowmya2910@users.noreply.github.com>
2019-04-26 03:02:46 -0600

    Update EmployeeManagement.java

--- OOAD_final/src/main/java/com/OOAD/employee/EmployeeManagement.java
+++ OOAD_final/src/main/java/com/OOAD/employee/EmployeeManagement.java
@@ -1,9 +1,17 @@
+/**
+ * @author Abhinav Gupta/Divya Athoopallil
+ */
 package com.OOAD.employee;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+/*
+ * This Controller contains handling REST APIs focused on
+ * dealing with employee.
+ */
 
 @Controller
 public class EmployeeManagement {
