d1feb0101cbf4b38d0b5e7cd25a18b8771751270
Sowmya Ramakrishnan <31675002+sowmya2910@users.noreply.github.com>
2019-04-26 03:11:25 -0600

    Update WelcomeController.java

--- OOAD_final/src/main/java/com/OOAD/login/WelcomeController.java
+++ OOAD_final/src/main/java/com/OOAD/login/WelcomeController.java
@@ -1,3 +1,14 @@
+
+/**
+* @author Abhinav Gupta/Divya Athoopallil
+*/
+
+
+/*
+* This Controller contains handling REST APIs focused on
+* dealing with welcome page after successful login.
+*/
+
 package com.OOAD.login;
 
 import org.springframework.security.core.context.SecurityContextHolder;
