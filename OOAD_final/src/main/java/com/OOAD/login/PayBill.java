7d7147ba9594ac9a08d6535547c997a24507ce7f
Sowmya Ramakrishnan <31675002+sowmya2910@users.noreply.github.com>
2019-04-26 03:08:50 -0600

    Comments

--- OOAD_final/src/main/java/com/OOAD/login/PayBill.java
+++ OOAD_final/src/main/java/com/OOAD/login/PayBill.java
@@ -1,3 +1,14 @@
+
+/**
+* @author Abhinav Gupta/Divya Athoopallil
+*/
+
+
+/*
+* This Controller contains handling REST APIs focused on
+* dealing with bill payment and confirmation
+*/
+
 package com.OOAD.login;
 
 import org.springframework.stereotype.Controller;
